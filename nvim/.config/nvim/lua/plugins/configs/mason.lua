2fbf7f78d47b3d608e63e84099291558d7d642a3
Rakesh Kumar <rakesh.kumar.132457@gmail.com>
2023-08-29 09:36:08 +0530

    fix: :bug: Added servers in mason-lspconfig

--- nvim/.config/nvim/lua/plugins/configs/mason.lua
+++ nvim/.config/nvim/lua/plugins/configs/mason.lua
@@ -6,7 +6,7 @@ M.setup = function()
 
     -- Setup for mason-lspconfig.nvim
     require('mason-lspconfig').setup({
-        ensure_installed = { 'clangd' } -- Add more servers here if needed
+        ensure_installed = { 'clangd', 'tsserver', 'gopls', 'pyright', 'rust_analyzer' } -- Add more servers here if needed
     })
 end
 
