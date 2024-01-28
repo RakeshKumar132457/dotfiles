572ccbd0f702e01670fdf9d047a8396119486ead
Rakesh Kumar <rakesh.kumar.132457@gmail.com>
2024-01-28 15:19:39 +0530

    Fixed truecolor issue for tmux

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -1,9 +1,11 @@
 local wezterm = require 'wezterm';
 
 return {
+    enable_tab_bar = false,
     font = wezterm.font("Iosevka Term Nerd Font Mono"),
-    default_prog = { "tmux", "new" },
+    default_prog = { "tmux", "-2" },
     font_size = 13.0,
     color_scheme = "Dracula",
-    underline_position = '-8px'
+    underline_position = '-8px',
+    term = "xterm-256color",
 }
