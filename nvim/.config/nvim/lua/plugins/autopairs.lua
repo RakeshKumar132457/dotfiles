70fc1f670388fe5878253e691ff161349e35d67b
Rakesh Kumar <rakesh.kumar.132457@gmail.com>
2024-02-20 23:19:45 +0530

    Modified settings for gitsigns

--- nvim/.config/nvim/lua/plugins/autopairs.lua
+++ nvim/.config/nvim/lua/plugins/autopairs.lua
@@ -8,10 +8,7 @@ return {
     },
     {
         "windwp/nvim-ts-autotag",
-        ft = {
-            'html', 'javascript', 'typescript', 'javascriptreact', 'typescriptreact', 'svelte', 'vue', 'tsx', 'jsx',
-            'rescript', 'xml', 'php', 'markdown', 'astro', 'glimmer', 'handlebars', 'hbs'
-        },
+        event = { "InsertEnter" },
         config = function()
             require('nvim-ts-autotag').setup({
                 filetypes = { 'html', 'javascript', 'typescript', 'javascriptreact', 'typescriptreact',
--- nvim/.config/nvim/lua/plugins/git.lua
+++ nvim/.config/nvim/lua/plugins/git.lua
@@ -3,7 +3,23 @@ return {
         'lewis6991/gitsigns.nvim',
         event = 'BufReadPost',
         config = function()
-            require('gitsigns').setup({})
+            local gitsigns = require('gitsigns');
+
+            gitsigns.setup({
+                current_line_blame = true
+            })
+
+            vim.keymap.set("n", "<leader>hp", function() gitsigns.preview_hunk_inline() end,
+                { desc = "[H]unk [P]review " })
+
+            vim.keymap.set("n", "<leader>hs", function() gitsigns.stage_hunk() end,
+                { desc = "[H]unk [S]tage " })
+
+            vim.keymap.set("n", "<leader>hr", function() gitsigns.reset_hunk() end,
+                { desc = "[H]unk [R]eset " })
+
+            vim.keymap.set("n", "<leader>hu", function() gitsigns.undo_stage_hunk() end,
+                { desc = "[H]unk [U]ndo" })
         end
     },
     {
@@ -13,7 +29,7 @@ return {
     },
     {
         "NeogitOrg/neogit",
-        cmd = {"Neogit"},
+        cmd = { "Neogit" },
         dependencies = {
             "nvim-lua/plenary.nvim",
             "sindrets/diffview.nvim",
