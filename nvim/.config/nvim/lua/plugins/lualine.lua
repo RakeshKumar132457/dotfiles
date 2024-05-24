db70dd623243ad473f02e039b281a936ca338a61
Rakesh Kumar <rakesh.kumar.132457@gmail.com>
2024-05-24 09:58:11 +0530

    Modified lualine

--- nvim/.config/nvim/lua/plugins/lualine.lua
+++ nvim/.config/nvim/lua/plugins/lualine.lua
@@ -11,7 +11,7 @@ return {
             sections = {
                 lualine_a = { 'mode' },
                 lualine_b = { 'branch', 'diff', 'diagnostics' },
-                lualine_c = { 'func' },
+                lualine_c = { '' },
                 lualine_x = { 'encoding', 'fileformat', 'filetype' },
                 lualine_y = { 'progress' },
                 lualine_z = { 'location' }
