4917346c2be89465dae1f06e761acea77e8dbd86
Rakesh Kumar <rakesh.kumar.132457@gmail.com>
2024-09-20 10:15:47 +0530

    Fixed typo in snippets

--- nvim/.config/nvim/lua/snippets/javascriptreact.lua
+++ nvim/.config/nvim/lua/snippets/javascriptreact.lua
@@ -4,7 +4,7 @@ local format = require("luasnip.extras.fmt").fmt
 return {
     luasnip.snippet('clc', format([[
         console.log('\\x1b[31m\\x1b[42m%s\\x1b[0m', JSON.stringify({}));
-    ]], { luasnip.insert_node(1, "Your variable here...") })),
+    ]], { luasnip.insert_node(1, "Input...") })),
 
     luasnip.snippet('cmt', format(
         [[
