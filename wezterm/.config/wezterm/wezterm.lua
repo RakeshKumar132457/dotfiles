b0a8e18c32de315e5da470aa3ecd80ca66b427e9
Rakesh Kumar <rakesh.kumar.132457@gmail.com>
2024-11-07 22:20:57 +0530

    Changed font size for wezterm

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -3,9 +3,8 @@ local wezterm = require 'wezterm';
 return {
     enable_tab_bar = false,
     font = wezterm.font("Iosevka Term Nerd Font Mono"),
-    -- default_prog = { "tmux", "-2" },
     default_prog = { "zellij" },
-    font_size = 13.0,
+    font_size = 12.0,
     color_scheme = "Dracula",
     underline_position = '-8px',
     term = "xterm-256color",
