5bb50972b1262a06031d92d40e62decd3bbb86f1
Rakesh Kumar <rakesh.kumar.132457@gmail.com>
2024-11-07 22:15:25 +0530

    Disabled bellsound in wezterm

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -3,10 +3,11 @@ local wezterm = require 'wezterm';
 return {
     enable_tab_bar = false,
     font = wezterm.font("Iosevka Term Nerd Font Mono"),
-    default_prog = { "tmux", "-2" },
-    -- default_prog = {"zellij"},
+    -- default_prog = { "tmux", "-2" },
+    default_prog = { "zellij" },
     font_size = 13.0,
     color_scheme = "Dracula",
     underline_position = '-8px',
     term = "xterm-256color",
+    audible_bell = "Disabled"
 }
