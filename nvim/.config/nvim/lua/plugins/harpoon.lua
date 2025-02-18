cd27cb18ffcb1d03afd2e094543444244fe63ef1
Rakesh Kumar <rakesh.kumar.132457@gmail.com>
2025-02-18 22:49:00 +0530

    Fix harpoon key conflict

--- nvim/.config/nvim/lua/plugins/harpoon.lua
+++ nvim/.config/nvim/lua/plugins/harpoon.lua
@@ -7,8 +7,8 @@ return {
         harpoon:setup()
         vim.keymap.set("n", "<leader>a", function() harpoon:list():add() end,
             { desc = "Harpoon Add File" })
-        vim.keymap.set("n", "<C-e>", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end,
-            { desc = "Harpoon Toggle Menu" })
+        vim.keymap.set("n", "<leader>hm", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end,
+            { desc = "[H]arpoon Toggle [M]enu" })
         vim.keymap.set("n", "<C-a>", function() harpoon:list():prev() end,
             { desc = "Harpoon Prev File" })
         vim.keymap.set("n", "<C-s>", function() harpoon:list():next() end,
