32f67d242a63b052a205feafff441048ab97cde6
Rakesh Kumar <rakesh.kumar.132457@gmail.com>
2025-02-19 10:03:03 +0530

    Modify blink.cmp config options

--- nvim/.config/nvim/lua/plugins/lsp.lua
+++ nvim/.config/nvim/lua/plugins/lsp.lua
@@ -161,7 +161,7 @@ return {
         version = '*',
         build = 'cargo build --release',
         opts = {
-            keymap = { preset = 'default' },
+            keymap = { preset = 'super-tab' },
             appearance = {
                 use_nvim_cmp_as_default = true,
                 nerd_font_variant = 'mono'
@@ -171,8 +171,18 @@ return {
             },
             snippets = {
                 preset = 'luasnip'
-            }
+            },
+            completion = {
+                menu = {
+                    draw = {
+                        columns = { { "label", "label_description", gap = 1 }, { "kind_icon", "kind", gap = 1 } }
+                    },
+                    border = "single",
+                },
+                documentation = { auto_show = true, auto_show_delay_ms = 500, window = { border = 'single' } },
+                ghost_text = { enabled = false },
+            },
+            signature = { enabled = true, window = { border = "single" } }
         }
     }
 }
-
