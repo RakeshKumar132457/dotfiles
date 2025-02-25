99f0f9fa09b24bbb55a7340d55bb7c40eda8e2a5
Rakesh Kumar <rakesh.kumar.132457@gmail.com>
2025-02-25 10:02:50 +0530

    Increase font size for wezterm

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -2,7 +2,7 @@ local wezterm = require 'wezterm';
 
 return {
     font = wezterm.font("IosevkaTerm Nerd Font Mono"),
-    font_size = 11.0,
+    font_size = 14.0,
     color_scheme = "Dracula",
     audible_bell = "Disabled"
 }
